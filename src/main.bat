d4ef79b5fe567b15a3e18daf14924a3fa7593479
Arghadip Das <arghadipdas@icloud.com>
2024-05-07 11:00:52 +0530

    added watermark remover

---
+++ src/main.bat
@@ -0,0 +1,18 @@
+@echo off
+
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo Error: Please run this script with administrator privileges.
+    exit /b
+)
+
+takeown /f %systemroot%\System32\SettingsEnvironment.Desktop.dll /a
+icacls %systemroot%\System32\SettingsEnvironment.Desktop.dll /grant Administrators:F
+
+gcc .\sunlock11.c -o run.exe
+.\run.exe
+del .\run.exe
+
+slmgr -rearm
+
+shutdown /r /f /t 0
