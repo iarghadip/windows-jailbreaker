05d361fc97103bd703024f03e72710c6cee748b3
Arghadip Das <arghadipdas@icloud.com>
2024-05-07 11:35:49 +0530

    added dependency auto download

--- main.bat
+++ main.bat
@@ -1,17 +1,37 @@
 @echo off
 
+:isCommandExists
+where %1 >nul 2>&1
+if "%errorlevel%" equ "0" (
+    exit /b 0
+) else (
+    exit /b 1
+)
+
 net session >nul 2>&1
-if %errorLevel% NEQ 0 (
+if "%errorLevel%" NEQ "0" (
     echo Error: Please run this script with administrator privileges.
     exit /b
+)
+
+call :isCommandExists scoop
+if not "%errorlevel%" equ "0" (
+    powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
+    powershell -Command "(New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh') | Invoke-Expression"
+)
+
+call :isCommandExists gcc
+if not "%errorlevel%" equ "0" (
+    scoop install main/gcc
 )
 
 takeown /f %systemroot%\System32\SettingsEnvironment.Desktop.dll /a
 icacls %systemroot%\System32\SettingsEnvironment.Desktop.dll /grant Administrators:F
 
-gcc .\main.c -o run.exe
-.\run.exe
-del .\run.exe
+call :isCommandExists .\run.exe
+if not "%errorlevel%" equ "0" (
+    gcc .\main.c -o run.exe
+)
 
 slmgr -rearm
 
