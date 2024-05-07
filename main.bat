91cb7235ed9c7dc00a6d54224764dc29767874c4
Arghadip Das <arghadipdas@icloud.com>
2024-05-07 11:40:50 +0530

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,38 +1,34 @@
 @echo off
 
-:isCommandExists
-where %1 >nul 2>&1
-if "%errorlevel%" equ "0" (
-    exit /b 0
-) else (
-    exit /b 1
-)
-
 net session >nul 2>&1
 if "%errorLevel%" NEQ "0" (
     echo Error: Please run this script with administrator privileges.
     exit /b
 )
 
-call :isCommandExists scoop
+where scoop >nul 2>&1
 if not "%errorlevel%" equ "0" (
+    echo Installing dependency Scoop...
     powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
     powershell -Command "(New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh') | Invoke-Expression"
 )
 
-call :isCommandExists gcc
+where gcc >nul 2>&1
 if not "%errorlevel%" equ "0" (
+    echo Installing dependency gcc...
     scoop install main/gcc
 )
 
 takeown /f %systemroot%\System32\SettingsEnvironment.Desktop.dll /a
 icacls %systemroot%\System32\SettingsEnvironment.Desktop.dll /grant Administrators:F
 
-call :isCommandExists .\run.exe
+where .\run.exe >nul 2>&1
 if not "%errorlevel%" equ "0" (
+    echo Compiling main.c...
     gcc .\main.c -o run.exe
 )
 
+.\run.exe
 slmgr -rearm
 
 shutdown /r /f /t 0
